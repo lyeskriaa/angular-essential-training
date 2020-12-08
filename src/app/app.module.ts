98712ffa02041ae686c8ed8ee2814f54f76b39c4
elyes kriaa <elyes.kriaa@sap.com>
2020-12-07 21:18:00 -0500

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
-import { FavoriteDirective } from './favorite.directive';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {MediaItemComponent} from './media-item.component';
+import { MediaItemComponent } from './media-item.component';
 import { MediaItemListComponent } from './media-item-list.component';
-
-
+import { FavoriteDirective } from './favorite.directive';
+import { CategoryListPipe } from './catergory-list.pipe'
+import { MediaItemFormComponent } from './media-item-form.component';
 
 @NgModule({
     //metadatas
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule
     ],
     //The declarations property is used to make components, directives, 
     //and pipes available to your module that don't come from another module. 
+    //this declarations property is for components, directives and pipes is for components,
+    // directives and pipes you want to make available to others within your module
     declarations: [
         AppComponent,
         MediaItemComponent,
         MediaItemListComponent,
-        FavoriteDirective
+        FavoriteDirective,
+        CategoryListPipe,
+        MediaItemFormComponent
     ],
     //The bootstrap property is used for a root module and will let Angular know which component or components will be 
     //the starting point for the bootstrap process. Basically, the entry point for your app code.
--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener ,Input } from '@angular/core';
 
 @Directive({
   selector: '[mwFavorite]'
@@ -7,4 +7,15 @@ export class FavoriteDirective {
   //To get a class applied to the host element that the directive is on, you can use another Angular decorator. 
   //HostBinding is used to bind a host element property to a directive property.
   @HostBinding('class.is-favorite') isFavorite = true;
+  @HostBinding('class.is-favorite-hovering') hovering = false;
+  @HostListener('mouseenter') onMouseEnter() {
+    this.hovering = true;
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.hovering = false;
+  }
+  // using input decorator for a setter method 
+  @Input() set mwFavorite(value) {
+    this.isFavorite = value;
+  }
 }
